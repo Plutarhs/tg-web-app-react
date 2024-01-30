4d38588528789984c1c555e781eca809b8c6a245
DogGhost <dogghost@MacBook-Pro-DogGhost.local>
2024-01-30 03:31:39 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { useEffect } from "react";
+import { useTelegram } from "./hooks/useTelegram";
+import Header from "./csomponents/Header/Header";
+
+function App() {
+    const { onToggleButton, tg } = useTelegram();
+
+    useEffect(() => {
+        tg.ready();
+    }, []);
+
+    return (
+        <div className="App">
+            <Header />
+            <button onClick={onToggleButton}>toggle</button>
+        </div>
+    );
+}
+
+export default App;
