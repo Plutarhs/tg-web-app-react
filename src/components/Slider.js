22a2302425eeb52b0c1b07b0459ff4e4abe43e61
DogGhost <dogghost@MacBook-Pro-DogGhost.local>
2024-01-30 15:56:36 +0300

    initial

---
+++ src/components/Slider.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Slider = () => {
+    return (
+        <div className="slider">
+            <img src="/images/kabum.jpg" alt="Banner 1" />
+            <img src="/images/kabum.jpg alt="Banner 2" />
+        </div>
+    );
+};
+
+export default Slider;
