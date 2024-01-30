cb41cf963dc003899d33aacb4dbf50b322d30167
DogGhost <dogghost@MacBook-Pro-DogGhost.local>
2024-01-30 05:12:48 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
-const TelegramBot = require('node-telegram-bot-api');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-const token = '6738484855:AAG-94s7v9wUvKkOp47PyMIJRhmARA92bGs';
-const webAppUrl = 'https://earnest-manatee-0dab0e.netlify.app';
-
-try {
-    const bot = new TelegramBot(token, { polling: true });
-
-    bot.on('message', async (msg) => {
-        const chatId = msg.chat.id;
-        const text = msg.text;
-
-        if (text === '/start') {
-            try {
-                await bot.sendMessage(chatId, 'приложение poizon', {
-                    reply_markup: {
-                        inline_keyboard: [
-                            [{ text: 'Сделать заказ', web_app: { url: webAppUrl } }]
-                        ]
-                    }
-                });
-            } catch (error) {
-                console.error('Error sending message:', error.message);
-            }
-        }
-    });
-} catch (error) {
-    console.error('Error creating TelegramBot:', error.message);
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
