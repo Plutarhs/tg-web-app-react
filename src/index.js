69aa358b59692d890a96195ff6cfa0f09ecfb5d6
DogGhost <dogghost@MacBook-Pro-DogGhost.local>
2024-01-30 04:01:39 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+const TelegramBot = require('node-telegram-bot-api');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const token = '6738484855:AAG-94s7v9wUvKkOp47PyMIJRhmARA92bGs';
+const webAppUrl = 'https://earnest-manatee-0dab0e.netlify.app';
+
+const bot = new TelegramBot(token, {polling: true});
+
+
+bot.on('message', async (msg)=> {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if(text === '/start') {
+        await bot.sendMessage(chatId, 'приложение poizon', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
+                ]
+            }
+        })
+    }
+})
 
