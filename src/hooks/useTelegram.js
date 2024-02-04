a73dd237e4d741f2c2ef3490971595f628f1b238
DogGhost <dogghost@MacBook-Pro-DogGhost.local>
2024-02-04 18:31:17 +0300

    initial

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -4,3 +4,19 @@ export function useTelegram() {
     const onClose = ()=> {
         tg.close()
     }
+
+    const onToggleButton = ()=> {
+        if(tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }
+
+    return {
+        onClose,
+        onToggleButton,
+        tg,
+        user: tg.initDataUnsafe?.user,
+    }
+}
