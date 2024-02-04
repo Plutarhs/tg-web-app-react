9d197ce8cfc71e71af8e05ce950af046444dcf6b
DogGhost <dogghost@MacBook-Pro-DogGhost.local>
2024-02-04 18:37:49 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -14,16 +14,6 @@ function App() {
         <div className="App">
             <Header />
             <button onClick={onToggleButton}>toggle</button>
-            <div className="banner">
-                <img src="/images/300x300.png" alt="Banner 1"/>
-                <h1>Banner 1</h1>
-                <p>This is the first banner.</p>
-            </div>
-            <div className="banner-right">
-                <img src="/images/300x300.png" alt="Banner 1"/>
-                <h1>Banner 2</h1>
-                <p>This is the second banner.</p>
-            </div>
         </div>
     );
 }
